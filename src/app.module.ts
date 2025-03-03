9cb1bb3e2da7a942febbeafe723583144983b9fe
tapocheg020 <ilya.guchick07@mail.ru>
2025-03-03 17:01:02 +0300

    guard middleware inceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
-import { Module, NestModule } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { LoggerMiddleware } from './conception/middleware'
 import { FlowersModule } from './flowers/flowers.module'
 @Module({
 	imports: [FlowersModule],
 })
-export class AppModule implements NestModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggerMiddleware).forRoutes('flowers')
+	}
+}
--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+	Controller,
+	Get,
+	Query,
+	UseGuards,
+	UseInterceptors,
+} from '@nestjs/common'
+import { AuthGuard } from '../conception/guard'
+import { LoggingInterceptor } from '../conception/interceptor'
+import { ParseIntPipe } from '../conception/pipe'
 import { FlowersService } from './flowers.service'
 
 @Controller('flowers')
+@UseInterceptors(LoggingInterceptor)
 export class FlowersController {
 	constructor(private readonly flowersService: FlowersService) {}
 
 	@Get()
-	findAll() {
+	@UseGuards(AuthGuard)
+	findAll(@Query('pageNumber', ParseIntPipe) pageNumber: number) {
 		return this.flowersService.findAll()
 	}
 }
