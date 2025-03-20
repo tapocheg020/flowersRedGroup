98d6ba18f24fcb25ffcef27ea118a6a2f87b5a3a
tapocheg020 <ilya.guchick07@mail.ru>
2025-03-20 16:47:55 +0300

    restartMoveGameYep

--- src/conception/guard.ts
+++ src/conception/guard.ts
@@ -10,11 +10,10 @@ export class AuthGuard implements CanActivate {
 	canActivate(context: ExecutionContext): boolean {
 		const request = context.switchToHttp().getRequest()
 		const isAuth = request.headers.authorization === 'secret'
-
+		console.log('Guard...')
 		if (!isAuth) {
 			throw new UnauthorizedException('Not authorization')
 		}
-
 		return isAuth
 	}
 }
